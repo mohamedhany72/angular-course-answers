da0760b6195217c82be0e3ee081bbd241dd00643
mohamedhany72 <medocapo304@yahoo.com>
2022-02-11 11:33:17 +0200

    module 2

---
+++ module2/refrence/scripts/app.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+    
+    angular.module('myApp', [])
+    .controller('myController', myController);
+    myController.$inject = ["$scope"];
+    function myController(){
+
+    };
+    
+    
+    
+})();
